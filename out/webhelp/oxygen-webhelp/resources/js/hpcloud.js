f0df98fa2734eb7dd065be4d3a9ca55bbab7d245
Gabriel McGoldrick <gabriel.mcgoldrick@hpe.com>
2015-11-19 15:54:39 +0000

    add cli-reference to test searching

--- out/webhelp/oxygen-webhelp/resources/js/hpcloud.js
+++ out/webhelp/oxygen-webhelp/resources/js/hpcloud.js
@@ -1,19 +1,18 @@
 
-      $(document).ready(function()
-      { 
-
-      
-      $('.gloss').each(function() {
-      $(this).qtip({
-      content: {
-      attr: 'title'
-      },
-      position: {
-      target: 'mouse',
-      adjust: { x: 5, y: 5 }
-      }
-      });
-      });
-      
-      } 
-      ); 
+$(document).ready(function () {
+    
+    
+    $('.gloss').each(function () {
+        $(this).qtip({
+            content: {
+                attr: 'title'
+            },
+            position: {
+                target: 'mouse',
+                adjust: {
+                    x: 5, y: 5
+                }
+            }
+        });
+    });
+});
--- out/webhelp/oxygen-webhelp/search/htmlFileList.js
+++ out/webhelp/oxygen-webhelp/search/htmlFileList.js
@@ -1,7 +1,8 @@
 //List of files which are indexed.
 fl = new Array();
 fl["0"]= "home.html";
-fl["1"]= "helion/index.html";
-fl["2"]= "helion/overview.html";
-fl["3"]= "helion/glossary/openstack.html";
+fl["1"]= "helion/cli_reference.html";
+fl["2"]= "helion/index.html";
+fl["3"]= "helion/overview.html";
+fl["4"]= "helion/glossary/openstack.html";
 var doStem = false
